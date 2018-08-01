03d534d24f36bb7373a6548de91ad92306fb7afb
Thomas Gardiner <thomgardiner@gmail.com>
2018-08-01 01:44:53 -0600

    basic landing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <p> hey </p>
-  
+      <Fragment>
+      <div class="col-md-12 main-container">
+        <div class="row">
+          <div class="intro">Hello, I'm Thomas!</div>
+        </div>
+        <div class="row">
+		      <div class="tagline">Full Stack Developer | Designer | Musician | Human Being</div>
+        </div>
+        <div class="row">
+          <div class="icons-social">
+          </div>
+        </div>
+      </div>
+      </Fragment>
   )}
 }
 
 export default App;
+
+{/* <div class="intro">Hello, I'm Thomas!</div>
+		<div class="tagline">Full Stack Developer | Designer | Musican | Human Being</div>
+		<div class="icons-social">
+			<a target="_blank" href="https://github.com/thomgardiner"><i class="fab fa-github"></i></a>
+			<a target="_blank" href="https://stackoverflow.com/story/thomgardiner"><i class="fab fa-stack-overflow"></i></a>
+			<a target="_blank" href="https://www.linkedin.com/in/thomas-gardiner-a824699/"><i class="fab fa-linkedin"></i></a>
+    </div> */}
