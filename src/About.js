79f71729ab564d15a677bae5abd97e24db37d1ae
Thomas Gardiner <thomgardiner@gmail.com>
2018-12-03 17:26:02 -0700

    layout changes

--- src/About.js
+++ src/About.js
@@ -13,25 +13,20 @@ let skillStyle = {
 
 class About extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
     render(){
         return(
             <Fragment>
-            <div class="row">
-            <div class="intro">Hello, I'm <span id="thomas">Thomas</span>.</div>
+          <div class="row">
+          <div id="image-container">
+          <img id="image" src="./thomas.jpeg"></img>
+          </div>
           </div>
           <div class="row">
-            <div class="tagline"> <span id="fullstack"> Full Stack Developer </span> | <span id="designer"> Designer </span> | <span id="musician"> Musician </span> | <span id="human"> Human </span> </div>
-          </div>
-          <div class="row">
-            <div class="icons-social">
-              <a target="_blank" href="https://github.com/thomgardiner"> <i class="fab fa-github"></i> </a>
-              <a target="_blank" href="https://www.linkedin.com/in/thomas-gardiner-a824699"> <i class="fab fa-linkedin"></i></a>
-              <a target="_blank" href="https://stackoverflow.com/users/10166761/thomgardiner?tab=profile"> <i class="fab fa-stack-overflow"></i></a>
-              <a target="_blank" href="mailto:thomgardiner@gmail.com"> <i class="far fa-envelope"></i> </a>
-            </div>
-          </div>
-          <div class="row">
-            <div class="about"> My name is <span id="thomasg"> Thomas Gardiner</span>. I'm a Full Stack Developer with a background in, and eye for, art and design. I like to <span id="build" >build stuff</span> and learn new things. I make music in my free time and love to travel. Let's build something cool, together! I have experience in: </div>
+            <div class="about"> My name is <span id="thomasg"> Thomas Gardiner</span>. I'm a Full Stack Developer with a background in, and eye for, art and design. I like to <span id="build" onClick={() => this.props.clickFunction()} >build stuff</span> and learn new things. I make music in my free time and love to travel. Let's build something cool, together! I have experience in: </div>
             <div class="row">
             <div class="about-skills"><span style={skillStyle}> Skills and Languages:</span> <p style={skills}> <span id="javascript"> Javascript </span> | <span id="node">Node </span> | <span id="react"> React </span> |<span id="mongo"> MongoDB </span> | <span id="mysql"> MySQL </span> | <span id="html"> HTML </span> | <span id="css"> CSS </span> | <span id="restful">Restful APIs </span> | <span id="response"> Responsive Design </span> | <span id="threed"> 3D Modeling </span> | <span id="mogo"> Motion Graphics </span> </p> </div>
             <div class="about-skills"><span style={skillStyle}> Software:</span> <p style={skills}> <span id="javascript"> 3DS Max </span> | <span class="skill"> Maya </span> | <span class="skill"> After Effects </span> | <span class="skill"> Photoshop </span> | <span class="skill"> Office </span> | <span class="skill"> Jira </span> </p> </div>
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import './App.css';
 import About from './About.js';
 import Portfolio from './Portfolio.js';
+import Home from './Home.js';
 
 let skillStyle = {
   fontWeight: "bold",
@@ -17,25 +18,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        currentView: "about",
+        currentView: "home",
     };
 
 }
+
 
   handleClickAbout(){
     this.setState({currentView: "about"});
   }
 
   handleClickPortfolio(){
+
     this.setState({currentView: "portfolio"});
+  }
+
+  handleClickHome(){
+
+    this.setState({currentView: "home"});
   }
 
 
   render() {
     return (
       <Fragment>
-      <div class="row">
+      <div class="row" id="navbar-container">
         <div class="nav">
+          <div id="thomas-nav" onClick={() => this.handleClickHome()}>Home</div>
+          <div id="divide"> | </div>
           <div id="about" onClick={() => this.handleClickAbout()}>About</div>
           <div id="divide"> | </div>
           <div id="projects" onClick={() => this.handleClickPortfolio()}>Portfolio</div>
@@ -44,7 +54,12 @@ class App extends Component {
       <div class="spaceStyle">
       <div class="row">
         <div class="col-md-12 main-container">
-        {this.state.currentView == 'about' ? <About /> : <Portfolio />}
+        {this.state.currentView == 'home' ? <Home /> : null }
+        {this.state.currentView == 'about' ? <About clickFunction={()=> this.handleClickPortfolio()} /> : null}
+        {this.state.currentView == 'portfolio' ? <Portfolio /> : null }
+        {() => {
+          
+        }}
         {/* <Portfolio /> */}
         </div>
       </div>
@@ -54,11 +69,3 @@ class App extends Component {
 }
 
 export default App;
-
-{/* <div class="intro">Hello, I'm Thomas!</div>
-		<div class="tagline">Full Stack Developer | Designer | Musican | Human Being</div>
-		<div class="icons-social">
-			<a target="_blank" href="https://github.com/thomgardiner"><i class="fab fa-github"></i></a>
-			<a target="_blank" href="https://stackoverflow.com/story/thomgardiner"><i class="fab fa-stack-overflow"></i></a>
-			<a target="_blank" href="https://www.linkedin.com/in/thomas-gardiner-a824699/"><i class="fab fa-linkedin"></i></a>
-    </div> */}
